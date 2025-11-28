5d4aea6c51cba1bdce9aa180840333b1834ac382
Mahadev Sagi <madhsa1972@gmail.com>
2025-11-28 12:14:17 -0500

    setting up data structures

--- src/CampusCompass.h
+++ src/CampusCompass.h
@@ -1,10 +1,36 @@
 #pragma once
 #include <string>
+#include<map>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<iomanip>
+#include<unordered_map>
+#include<unordered_set>
+#include<stack>
+#include<set>
 
 using namespace std;
-
+struct Student {
+    string name;
+    int id;
+    unordered_set<string> classes;
+    int residenceID;
+};
+struct Class {
+    string classCode;
+    int locationID;
+    string startTime;
+    string endTime;
+};
 class CampusCompass {
 private:
+    unordered_map<int,string> locations;
+    unordered_map<int,vector<pair<int,int>>> graph;
+    unordered_map<int, Student> students;
+    unordered_map<string, Class> classes;
+    set<pair<int,int>> closedEdges;
+
     // Think about what member variables you need to initialize
     // perhaps some graph representation?
 public:
