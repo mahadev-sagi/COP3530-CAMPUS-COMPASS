363f2d3a08bb1a153f659987f87937aff826523a
Mahadev Sagi <madhsa1972@gmail.com>
2025-11-29 23:15:25 -0500

    more progress

--- src/CampusCompass.cpp
+++ src/CampusCompass.cpp
@@ -12,12 +12,54 @@ CampusCompass::CampusCompass() {
 
 bool CampusCompass::ParseCSV(const string &edges_filepath, const string &classes_filepath) {
     // return boolean based on whether parsing was successful or not
-
+    string line;
     ifstream classFile(classes_filepath);
     if (!classFile.is_open()) {
         cerr<<"Error opening file "<<classes_filepath<<endl;
         return false;
     }
+
+    getline(classFile, line);
+    while (getline(classFile,line)) {
+        stringstream ss(line);
+        string classCode, startTime, endTime, locationStr;
+        getline(ss, classCode, ',');
+        getline(ss,locationStr, ',');
+        getline(ss, startTime, ',');
+        getline(ss, endTime);
+        int locationID = stoi(locationStr);
+        Class newClass;
+        newClass.classCode = classCode;
+        newClass.startTime = startTime;
+        newClass.endTime = endTime;
+        newClass.locationID = locationID;
+        classes[classCode] = newClass;
+    }
+    ifstream edgesFile(edges_filepath);
+    if (!edgesFile.is_open()) {
+        cerr<<"Error opening file "<<edges_filepath<<endl;
+        return false;
+    }
+    while (getline(edgesFile,line)) {
+        stringstream ss(line);
+        string location1, location2, name1, name2, timeStr;
+        getline(ss, location1, ',');
+        getline(ss, location2, ',');
+        getline(ss, name1, ',');
+        getline(ss, name2, ',');
+        getline(ss, timeStr);
+
+        int loc1 = stoi(location1);
+        int loc2 = stoi(location2);
+        int time = stoi(timeStr);
+        locations[loc1] = name1;
+        locations[loc2] = name2;
+
+        graph[loc1].push_back({loc2,time});
+        graph[loc2].push_back({loc1,time});
+
+    }
+
 
     return true;
 }
@@ -25,7 +67,33 @@ bool CampusCompass::ParseCSV(const string &edges_filepath, const string &classes
 bool CampusCompass::ParseCommand(const string &command) {
     // do whatever regex you need to parse validity
     // hint: return a boolean for validation when testing. For example:
-    bool is_valid = true; // replace with your actual validity checking
+    //bool is_valid = true; // replace with your actual validity checking
+    stringstream ss(command);
+    string Command;
+    ss>>Command;
+    if (Command == "insert") {
 
-    return is_valid;
+    }
+    else if (Command == "remove") {
+        int studentID;
+        ss >> studentID;
+        if (to_string(studentID).length() != 8) {
+            cout <<"unsuccessful"<<endl;
+            return false;
+        }
+
+        if (students.find(studentID) != students.end()) {
+            students.erase(studentID);
+            cout << "successful"<<endl;
+            return true;
+        }else {
+            cout << "unsuccessful"<<endl;
+            return false;
+        }
+
+    }
+    else if (Command =="dropClass"){}
+
+    return true;
+
 }
